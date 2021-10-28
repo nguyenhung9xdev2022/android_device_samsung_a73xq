ba3a0ea794a74b7f644a66c880e6b3b58f6fe621
Adarshgrewal <adarshgrewal@gmail.com>
2021-10-29 03:33:05 +0530

    a73xq: Define Boot Animation res

--- device.mk
+++ device.mk
@@ -23,6 +23,10 @@ PRODUCT_PACKAGES += \
 # API level
 PRODUCT_SHIPPING_API_LEVEL := 30
 
+# Boot animation
+TARGET_SCREEN_HEIGHT := 2400
+TARGET_SCREEN_WIDTH := 1080
+
 # Fastbootd
 PRODUCT_PACKAGES += \
     fastbootd
