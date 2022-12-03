f2c145b29248ce0cb6241de58273dc57f8d023c8
nguyenhung9xdev2022 <101638735+nguyenhung9xdev2022@users.noreply.github.com>
2022-12-03 15:52:03 +0700

    a73xq: Configure partitions

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -11,6 +11,9 @@ DEVICE_PATH := device/samsung/a73xq
 # Inherit from proprietary files
 include vendor/samsung/a73xq/BoardConfigVendor.mk
 
+# No A/B
+AB_OTA_UPDATER := false
+
 # Architecture
 TARGET_ARCH := arm64
 TARGET_ARCH_VARIANT := armv8-a
@@ -47,6 +50,34 @@ TARGET_KERNEL_SOURCE := kernel/samsung/a73xq
 BOARD_KERNEL_CMDLINE += console=null androidboot.hardware=qcom androidboot.memcg=1 lpm_levels.sleep_disabled=1 video=vfb:640x400,bpp=32,memsize=3072000 msm_rtb.filter=0x237 service_locator.enable=1 androidboot.usbcontroller=a600000.dwc3 swiotlb=0 loop.max_part=7 cgroup.memory=nokmem,nosocket firmware_class.path=/vendor/firmware_mnt/image pcie_ports=compat loop.max_part=7 iptable_raw.raw_before_defrag=1 ip6table_raw.raw_before_defrag=1 printk.devkmsg=on
 BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 
+BOARD_DTBOIMG_PARTITION_SIZE       := 25165824
+BOARD_BOOTIMAGE_PARTITION_SIZE     := 100663296
+BOARD_VENDOR_BOOTIMAGE_PARTITION_SIZE := 100663296
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 81788928
+BOARD_CACHEIMAGE_PARTITION_SIZE    := 629145600
+
+# Partitions
+BOARD_SUPER_PARTITION_SIZE                      := 10385096704
+BOARD_SUPER_PARTITION_GROUPS                    := samsung_dynamic_partitions
+BOARD_SAMSUNG_DYNAMIC_PARTITIONS_PARTITION_LIST := system vendor product odm
+BOARD_SAMSUNG_DYNAMIC_PARTITIONS_SIZE           := 10380902400 # BOARD_SUPER_PARTITION_SIZE-4MiB
+
+BOARD_USERDATAIMAGE_FILE_SYSTEM_TYPE := f2fs
+BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE   := ext4
+BOARD_SYSTEMIMAGE_FILE_SYSTEM_TYPE   := ext4
+BOARD_PRODUCTIMAGE_FILE_SYSTEM_TYPE  := ext4
+BOARD_ODMIMAGE_FILE_SYSTEM_TYPE      := ext4
+BOARD_CACHEIMAGE_FILE_SYSTEM_TYPE    := ext4
+
+BOARD_USES_METADATA_PARTITION := true
+
+TARGET_COPY_OUT_VENDOR := vendor
+TARGET_COPY_OUT_PRODUCT := product
+TARGET_COPY_OUT_ODM := odm
+
+TARGET_USERIMAGES_USE_EXT4 := true
+TARGET_USERIMAGES_USE_F2FS := true
+
 # Platform
 TARGET_BOARD_PLATFORM := lahaina
 
@@ -56,6 +87,7 @@ BOARD_USES_QCOM_HARDWARE := true
 # Recovery
 BOARD_HAS_DOWNLOAD_MODE := true
 BOARD_INCLUDE_RECOVERY_DTBO := true
+TARGET_USES_MKE2FS := true
 
 # Screen density
 TARGET_SCREEN_DENSITY := 450
