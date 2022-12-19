0bf69a06f45eaf35341b1f920d3f3275d81eea24
nguyenhung9xdev2022 <101638735+nguyenhung9xdev2022@users.noreply.github.com>
2022-12-19 07:47:38 +0700

    a73xq: Use stock health blobs

--- device.mk
+++ device.mk
@@ -137,6 +137,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.opengles.aep.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.opengles.aep.xml \
     frameworks/native/data/etc/android.software.opengles.deqp.level-2020-03-01.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.opengles.deqp.level.xml
 
+# Health
+PRODUCT_PACKAGES += \
+    android.hardware.health@2.1.vendor
+
 # HIDL
 PRODUCT_PACKAGES += \
     libhidltransport.vendor \
